85b6257af7f951fb79aa0c8d57b22b372918064e
BlackOrangee <33674663+BlackOrangee@users.noreply.github.com>
2024-09-30 20:28:38 +0300

    b

---
+++ Back-end/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace DENMAP_SERVER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = new Uri("http://localhost:1234");
+
+            using (var host = new NancyHost(uri))
+            {
+                host.Start();
+                Console.WriteLine($"NancyFX is running on {uri}. Press Enter to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
