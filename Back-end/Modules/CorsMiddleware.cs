6508c9916b52d698bac7625094279cca597a9ec9
Denys <33674663+BlackOrangee@users.noreply.github.com>
2024-10-12 10:30:00 +0300

    Create CorsMiddleware.cs

---
+++ Back-end/Modules/CorsMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Nancy;
+
+namespace DENMAP_SERVER.Modules
+{
+    public class CorsMiddleware : NancyModule
+    {
+        public CorsMiddleware()
+        {
+            After.AddItemToEndOfPipeline(AddCorsHeaders);
+        }
+
+        private void AddCorsHeaders(NancyContext context)
+        {
+            context.Response.WithHeader("Access-Control-Allow-Origin", "*")
+                            .WithHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                            .WithHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+            
+            if (context.Request.Method == "OPTIONS")
+            {
+                context.Response.StatusCode = HttpStatusCode.OK;
+            }
+        }
+    }
+
+}
