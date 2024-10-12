3aba2a00189a7a8fd7508c5ae82c0f3e991d799f
Denys <33674663+BlackOrangee@users.noreply.github.com>
2024-10-12 12:02:07 +0300

    Update AuthController.cs

--- Back-end/Controller/AuthController.cs
+++ Back-end/Controller/AuthController.cs
@@ -20,6 +20,22 @@ namespace DENMAP_SERVER.Controller
 
         public AuthController()
         {
+            Options("/*", args =>
+            {
+                return new Response()
+                    .WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                    .WithHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+            });
+
+            After.AddItemToEndOfPipeline(ctx =>
+            {
+                ctx.Response
+                    .WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                    .WithHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+            });
+
             Post(_basePath + "/register", args =>
             {
 
