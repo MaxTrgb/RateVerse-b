0a62060674f23f1ddc96cfd0755440844b0cc3b2
Denys <33674663+BlackOrangee@users.noreply.github.com>
2024-10-12 10:43:57 +0300

    Update CorsMiddleware.cs

--- Back-end/Modules/CorsMiddleware.cs
+++ Back-end/Modules/CorsMiddleware.cs
@@ -6,19 +6,21 @@ namespace DENMAP_SERVER.Modules
     {
         public CorsMiddleware()
         {
-            After.AddItemToEndOfPipeline(AddCorsHeaders);
-        }
-
-        private void AddCorsHeaders(NancyContext context)
-        {
-            context.Response.WithHeader("Access-Control-Allow-Origin", "*")
-                            .WithHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-                            .WithHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
-            
-            if (context.Request.Method == "OPTIONS")
+            Options("/*", args =>
             {
-                context.Response.StatusCode = HttpStatusCode.OK;
-            }
+                return new Response()
+                    .WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                    .WithHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+            });
+
+            After.AddItemToEndOfPipeline(ctx =>
+            {
+                ctx.Response
+                    .WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                    .WithHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+            });
         }
     }
 
