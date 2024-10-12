f003f9f2d82830f383fa1e04cb3a8751b014bb11
Denys <33674663+BlackOrangee@users.noreply.github.com>
2024-10-12 12:02:10 +0300

    Update CommentController.cs

--- Back-end/Controller/CommentController.cs
+++ Back-end/Controller/CommentController.cs
@@ -25,6 +25,21 @@ namespace DENMAP_SERVER.Controller
 
         public CommentController()
         {
+            Options("/*", args =>
+            {
+                return new Response()
+                    .WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                    .WithHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+            });
+
+            After.AddItemToEndOfPipeline(ctx =>
+            {
+                ctx.Response
+                    .WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                    .WithHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+            });
 
             Get(_basePath + "/", args =>
             {
